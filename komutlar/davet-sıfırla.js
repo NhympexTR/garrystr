9981e61746f7c7a0004e805bc1399efdb6264827
NhympexTR <77833482+NhympexTR@users.noreply.github.com>
2021-01-22 13:22:39 +0300

    Add files via upload

---
+++ komutlar/davet-sıfırla.js
@@ -0,0 +1,70 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+module.exports.run = async (bot, message, args) => {
+  let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
+  if (!message.member.hasPermission("KICK_MEMBERS")) {
+    const embed = new Discord.MessageEmbed()
+      .setDescription("```Ne yazık ki bu komutu kullanmaya yetkin yok.```")
+      .setColor("RED")
+  .setAuthor(`Hatalı Giriş`, message.author.avatarURL)
+    .setFooter(`${message.author.tag} Tarafından İstendi`, message.author.avatarURL)
+  .setTimestamp()     
+
+    message.channel.send(embed);
+    return;
+  }
+
+  let u = message.mentions.users.first();
+  if (!u) {
+    return message.channel.send(
+      new Discord.MessageEmbed()
+        .setDescription("Lütfen daveti sıfırlanacak kişiyi etiketleyiniz!")
+        .setColor("RED")
+  .setAuthor(`Hatalı Giriş`, message.author.avatarURL)
+    .setFooter(`${message.author.tag} Tarafından İstendi`, message.author.avatarURL)
+  .setTimestamp()     
+    );
+  }
+
+  const Lrows = new Discord.MessageEmbed()
+    .setColor("GREEN")
+    .setDescription(
+      `${u} Adlı şahsın davetlerinin sıfırlanmasını onaylıyor musunuz?`
+    )
+  .setAuthor(`Onaylama İsteği`, message.author.avatarURL)
+    .setFooter(`${message.author.tag} Tarafından İstendi`, message.author.avatarURL)
+  .setTimestamp()     
+  message.channel.send(Lrows).then(async function(sentEmbed) {
+    const emojiArray = ["✅"];
+    const filter = (reaction, user) =>
+      emojiArray.includes(reaction.emoji.name) && user.id === message.author.id;
+    await sentEmbed.react(emojiArray[0]).catch(function() {});
+    var reactions = sentEmbed.createReactionCollector(filter, {
+      time: 30000
+    });
+    reactions.on("end", () => sentEmbed.edit("İşlem iptal oldu!"));
+    reactions.on("collect", async function(reaction) {
+      if (reaction.emoji.name === "✅") {
+        message.channel.send(
+          `İşlem onaylandı! ${u} adlı şahsın davetleri sıfırlandı!`
+        );
+
+        db.delete(`davet_${message.author.id}_${message.guild.id}`);
+      }
+    });
+  });
+};
+
+module.exports.conf = {
+  aliases: ["davetsıfırla"],
+  permLevel: 2,
+  enabled: true,
+  guildOnly: true,
+  kategori: "moderasyon"
+};
+
+module.exports.help = {
+  name: "davet-sıfırla",
+  description: "davet-sıfırla",
+  usage: "davet-sıfırla"
+};
---
+++ komutlar/rütbe-liste.js
@@ -0,0 +1,60 @@
+const Discord = require("discord.js"),
+  db = require("quick.db");
+
+exports.run = async (client, message, args) => {
+  let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
+  let veri = await db.fetch(`rol1_${message.guild.id}`);
+  let veri12 = await db.fetch(`roldavet1_${message.guild.id}`);
+  let veri21 = await db.fetch(`roldavet2_${message.guild.id}`);
+  let veri2 = await db.fetch(`rol2_${message.guild.id}`);
+
+  if (!veri) {
+    const embed = new Discord.MessageEmbed()
+      .setDescription(`Zaten herhangi bir rütbe ayarlanmamış!`)
+      .setColor("RED")
+  .setAuthor(`Hatalı Giriş`, message.author.avatarURL)
+    .setFooter(`${message.author.tag} Tarafından İstendi`, message.author.avatarURL)
+  .setTimestamp()     
+
+    message.channel.send(embed);
+  }
+  if (veri) {
+    if (!veri2) {
+      const Lrows = new Discord.MessageEmbed()
+        .setDescription(
+          `<@&${message.guild.roles.cache.get(veri).id}>: ${veri12} Davet!`
+        )
+    .setColor("GREEN")
+  .setAuthor(`Rütbe Liste`, message.author.avatarURL)
+    .setFooter(`${message.author.tag} Tarafından İstendi`, message.author.avatarURL)
+  .setTimestamp()     
+
+      message.channel.send(Lrows);
+      return;
+    } else {
+      const Lrows = new Discord.MessageEmbed()
+        .setDescription(
+          `<@&${message.guild.roles.cache.get(veri).id}>: **${veri12} Davet!**
+          <@&${message.guild.roles.cache.get(veri).id}>: **${veri21} Davet!**`
+        )
+    .setColor("GREEN")
+  .setAuthor(`Rank`, message.author.avatarURL)
+    .setFooter(`${message.author.tag} Tarafından İstendi`, message.author.avatarURL)
+  .setTimestamp()     
+
+      message.channel.send(Lrows);
+      return;
+    }
+  }
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['rütbeliste'],
+  permLevel: 2
+};
+exports.help = {
+  name: "rütbe-liste",
+  description: "rütbe-liste",
+  usage: "rütbe-liste"
+};
